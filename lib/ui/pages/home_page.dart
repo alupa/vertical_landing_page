1dfcd0a7e75f7334016e7506b14fcf0934421c3d
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-06-28 14:28:04 -0400

    Crear gradiente de fondo

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_landing_page/ui/views/about_view.dart';
+import 'package:vertical_landing_page/ui/views/contact_view.dart';
+import 'package:vertical_landing_page/ui/views/home_view.dart';
+import 'package:vertical_landing_page/ui/views/location_view.dart';
+import 'package:vertical_landing_page/ui/views/pricing_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink,
+    return Scaffold(
+      body: Container(
+        decoration: buildBoxDecoration(),
+        child: Stack(
+          children: [
+            _HomeBody(),
+            Positioned(
+              right: 20,
+              top: 20,
+              child: Container(
+                width: 150,
+                height: 50,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration buildBoxDecoration() => BoxDecoration(
+      gradient: LinearGradient(
+          colors: [Colors.pink, Colors.purpleAccent],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [0.5, 0.5]));
+}
+
+class _HomeBody extends StatelessWidget {
+  const _HomeBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+      scrollDirection: Axis.vertical,
+      children: [
+        HomeView(),
+        AboutView(),
+        PricingView(),
+        ContactView(),
+        LocationView()
+      ],
     );
   }
 }
