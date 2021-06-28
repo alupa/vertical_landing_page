6e2603281ac42f71a36131c3c863c5fa18369637
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-06-28 17:05:42 -0400

    Animaciones del menú

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CustomAppMenu extends StatelessWidget {
+class CustomAppMenu extends StatefulWidget {
   const CustomAppMenu({Key? key}) : super(key: key);
+
+  @override
+  _CustomAppMenuState createState() => _CustomAppMenuState();
+}
+
+class _CustomAppMenuState extends State<CustomAppMenu>
+    with SingleTickerProviderStateMixin {
+  bool isOpen = false;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
-        onTap: () => print('click'),
+        onTap: () {
+          isOpen ? controller.reverse() : controller.forward();
+          setState(() => isOpen = !isOpen);
+        },
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 10),
           width: 150,
@@ -17,13 +36,22 @@ class CustomAppMenu extends StatelessWidget {
           color: Colors.black,
           child: Row(
             children: [
+              AnimatedContainer(
+                duration: Duration(milliseconds: 200),
+                curve: Curves.easeInOut,
+                width: isOpen ? 50 : 0,
+              ),
               Text('Menú',
                   style: GoogleFonts.roboto(
                     color: Colors.white,
                     fontSize: 18,
                   )),
               Spacer(),
-              Icon(Icons.menu, color: Colors.white),
+              AnimatedIcon(
+                icon: AnimatedIcons.menu_close,
+                progress: controller,
+                color: Colors.white,
+              )
             ],
           ),
         ),
