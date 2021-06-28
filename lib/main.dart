40630a433c21e17cb86f2947357ae5bd6004d2cd
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-06-28 13:28:22 -0400

    Home Page

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:vertical_landing_page/router/router.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Flurorouter.configureRoutes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Landing Page',
+      initialRoute: '/home',
+      onGenerateRoute: Flurorouter.router.generator,
+    );
+  }
+}
