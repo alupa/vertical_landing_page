fb3e1e6914976392b81317250af3f72fb4258777
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-06-28 18:50:13 -0400

    Detalles esteticos del menú

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:vertical_landing_page/ui/shared/custom_menu_item.dart';
 
 class CustomAppMenu extends StatefulWidget {
   const CustomAppMenu({Key? key}) : super(key: key);
@@ -32,29 +33,61 @@ class _CustomAppMenuState extends State<CustomAppMenu>
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 10),
           width: 150,
-          height: 50,
+          height: isOpen ? 310 : 50,
           color: Colors.black,
-          child: Row(
+          child: Column(
             children: [
-              AnimatedContainer(
-                duration: Duration(milliseconds: 200),
-                curve: Curves.easeInOut,
-                width: isOpen ? 50 : 0,
-              ),
-              Text('Menú',
-                  style: GoogleFonts.roboto(
-                    color: Colors.white,
-                    fontSize: 18,
-                  )),
-              Spacer(),
-              AnimatedIcon(
-                icon: AnimatedIcons.menu_close,
-                progress: controller,
-                color: Colors.white,
-              )
+              _MenuTitle(isOpen: isOpen, controller: controller),
+              if (isOpen) ...[
+                CustomMenuItem(delay: 0, text: 'Home', onPressed: () {}),
+                CustomMenuItem(delay: 30, text: 'About', onPressed: () {}),
+                CustomMenuItem(delay: 60, text: 'Pricing', onPressed: () {}),
+                CustomMenuItem(delay: 90, text: 'Contact', onPressed: () {}),
+                CustomMenuItem(delay: 120, text: 'Location', onPressed: () {}),
+                SizedBox(height: 10)
+              ]
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _MenuTitle extends StatelessWidget {
+  const _MenuTitle({
+    Key? key,
+    required this.isOpen,
+    required this.controller,
+  }) : super(key: key);
+
+  final bool isOpen;
+  final AnimationController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 50,
+      child: Row(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            width: isOpen ? 45 : 0,
+          ),
+          Text('Menú',
+              style: GoogleFonts.roboto(
+                color: Colors.white,
+                fontSize: 18,
+              )),
+          Spacer(),
+          AnimatedIcon(
+            icon: AnimatedIcons.menu_close,
+            progress: controller,
+            color: Colors.white,
+          )
+        ],
       ),
     );
   }
