9a43f134cdbe80312e2113067abab8a469d7eefa
Alvaro Lupa <alvaro.lupa@gmail.com>
2021-07-04 00:30:15 -0400

    Añadir un listener de movimiento del PageController

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -5,10 +5,19 @@ import 'package:universal_html/html.dart' as html;
 class PageProvider extends ChangeNotifier {
   PageController scrollController = new PageController();
   List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
+  int _currentIndex = 0;
 
   createScrollController(String routeName) {
     this.scrollController =
         new PageController(initialPage: getPageIndex(routeName));
+
+    this.scrollController.addListener(() {
+      final index = (this.scrollController.page ?? 0).round();
+      if (index != _currentIndex) {
+        html.window.history.pushState(null, 'none', '#/${_pages[index]}');
+        _currentIndex = index;
+      }
+    });
   }
 
   int getPageIndex(String routeName) {
@@ -17,7 +26,7 @@ class PageProvider extends ChangeNotifier {
 
   goTo(int index) {
     // final routeName = _pages[index];
-    html.window.history.pushState(null, 'none', '#/${_pages[index]}');
+
     scrollController.animateToPage(index,
         duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
   }
